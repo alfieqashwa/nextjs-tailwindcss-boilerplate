ef3d0958c427dbd11d4313575a1e23ba62cdea2e
alfieqashwa <alfieqashwa@gmail.com>
2020-12-19 22:48:03 +0700

    1. installed prisma, 2. added custom colors, 3. modified home page,

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,18 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>NextJS + Tailwind + Prisma + TypeScript</title>
       </Head>
 
       <main>
-        <h1>
-          Welcome to NextJS with Tailwind 2.0
+        <h1 className='text-4xl text-center text-'>
+          Welcome to NextJS with Tailwind 2.0 and Prisma 2 and ofcourse,
+          TypeScript 4
         </h1>
-        <br />
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js', './pages/**/*.tsx', './components/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      gray: colors.coolGray,
+      red: colors.rose,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.lightBlue,
+      indigo: colors.indigo,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      fuchsia: colors.fuchsia
+    },
     extend: {},
   },
   variants: {
